bf2c7ec6cfe68784a2b6745d54ad3f13c5e9d348
s2236141 <p.vaish@student.utwente.nl>
2020-10-15 21:40:21 +0100

    Add: friendly Advertisement
    Also added the LED part for MVP

--- moodify-raspi/config.sh
+++ moodify-raspi/config.sh
@@ -9,7 +9,7 @@ sudo hciconfig hci0 up
 
 sudo bluetoothctl -- power on & sudo bluetoothctl -- advertise on & sudo bluetoothctl -- discoverable on &
 sleep 0.2
-sudo python3 moodify-gatt-server
+sudo python3 moodify-gatt-server.py
 sleep 0.2
 sudo bluetoothctl -- advertise off & sudo bluetoothctl -- discoverable off & sudo bluetoothctl -- power off &
 echo Completed...
