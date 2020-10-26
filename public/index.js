0bc2af4fc129acd1bb700f1ee3d1de3900bb4b93
s1992988 <m.m.c.romeijn@student.utwente.nl>
2020-10-26 17:16:59 +0100

    Added Safari

--- public/index.js
+++ public/index.js
@@ -71,14 +71,15 @@ function compatibilityCtr(){
     majorVersion = parseInt(navigator.appVersion,10);
     }
 
-    if(browserName == "Chrome" && (majorVersion < 56 || (majorVersion < 70 && OSName == "Windows 10"))){
+    if(browserName == "Safari"){
+        alert("This browser is not supported by our product");}
+    else if(browserName == "Chrome" && (majorVersion < 56 || (majorVersion < 70 && OSName == "Windows 10"))){
         alert("Please update Google Chrome");
     }else if(browserName == "Edge" && majorVersion > 79){
         alert("The new version of Edge are not compatible, use version 79 or lower");}
     else if (browserName == "Opera" && (majorVersion < 43 || (majorVersion < 57 && OSName == "Windows 10") )){
         alert("Please update Opera");}
     }
-
 
 
 function loadIn(){
