047b636b0694b032e2fe572ef7e552d5d4b5e369
s2236141 <p.vaish@student.utwente.nl>
2020-10-26 15:37:10 +0000

    Add -y for auto accept prompts in shellscript

--- moodify-raspi/setup-mic.sh
+++ moodify-raspi/setup-mic.sh
@@ -2,5 +2,5 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install libportaudio2 libportaudiocpp0 portaudio19-dev
+sudo apt-get install libportaudio2 libportaudiocpp0 portaudio19-dev -y
 sudo pip3 install pyaudio
