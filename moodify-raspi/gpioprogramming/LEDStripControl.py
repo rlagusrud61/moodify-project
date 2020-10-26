0f0dd7bb04ee89947a1b74d93fbb9e94f0edddad
Puru Vaish <puruvaish24@outlook.com>
2020-10-26 16:34:38 +0100

    Add: Microphone Test and Music Mode Feature connected to LEDStripControl.py.

--- moodify-raspi/gpioprogramming/LEDStripControl.py
+++ moodify-raspi/gpioprogramming/LEDStripControl.py
@@ -5,13 +5,51 @@ import pyaudio
 import board
 import neopixel
 import wave
-import numpy
+import numpy as np
 
 OFF = (0, 0, 0)
 
 
 def brightnessAdjustedColour(colour, brightness):
     return tuple(math.floor(brightness * colour) for colour in colour)
+
+
+def wavelength_to_rgb(wavelength, gamma = 0.8)
+    wavelength = float(wavelength)
+    if 380 <= wavelength <= 440:
+        attenuation = 0.3 + 0.7 * (wavelength - 380) / (440 - 380)
+        R = ((-(wavelength - 440) / (440 - 380)) * attenuation) ** gamma
+        G = 0.0
+        B = (1.0 * attenuation) ** gamma
+    elif 440 <= wavelength <= 490:
+        R = 0.0
+        G = ((wavelength - 440) / (490 - 440)) ** gamma
+        B = 1.0
+    elif 490 <= wavelength <= 510:
+        R = 0.0
+        G = 1.0
+        B = (-(wavelength - 510) / (510 - 490)) ** gamma
+    elif 510 <= wavelength <= 580:
+        R = ((wavelength - 510) / (580 - 510)) ** gamma
+        G = 1.0
+        B = 0.0
+    elif 580 <= wavelength <= 645:
+        R = 1.0
+        G = (-(wavelength - 645) / (645 - 580)) ** gamma
+        B = 0.0
+    elif 645 <= wavelength <= 750:
+        attenuation = 0.3 + 0.7 * (750 - wavelength) / (750 - 645)
+        R = (1.0 * attenuation) ** gamma
+        G = 0.0
+        B = 0.0
+    else:
+        R = 0.0
+        G = 0.0
+        B = 0.0
+    R *= 255
+    G *= 255
+    B *= 255
+    return tuple([int(R), int(G), int(B)])
 
 
 class StripControl:
@@ -76,95 +114,57 @@ class StripControl:
         if self.__e.isSet():
             print("Turning off the music")
             self.__e.clear()
-	
-	def soundAnalyze(stream)
-		data = stream.read(8192, exception_on_overflow=False)
-		waveData = wave.struct.unpack("%dh"%(8192), data)
-		npArrayData = np.array(waveData)
-		volume = npArrayData[1:].argmax() + 1
-		fftData=np.abs(np.fft.rfft(npArrayData))
-		which = fftData[1:].argmax() + 1
-		thefreq = which*44100/8192
-		return[npArrayData[volume], thefreq]
-	
-	def wavelength_to_rgb(wavelength, gamma = 0.8)
-		wavelength = float(wavelength)
-		if wavelength >= 380 and wavelength <= 440:
-			attenuation = 0.3 + 0.7 * (wavelength - 380) / (440 - 380)
-			R = ((-(wavelength - 440) / (440 - 380)) * attenuation) ** gamma
-			G = 0.0
-			B = (1.0 * attenuation) ** gamma
-		elif wavelength >= 440 and wavelength <= 490:
-			R = 0.0
-			G = ((wavelength - 440) / (490 - 440)) ** gamma
-			B = 1.0
-		elif wavelength >= 490 and wavelength <= 510:
-			R = 0.0
-			G = 1.0
-			B = (-(wavelength - 510) / (510 - 490)) ** gamma
-		elif wavelength >= 510 and wavelength <= 580:
-			R = ((wavelength - 510) / (580 - 510)) ** gamma
-			G = 1.0
-			B = 0.0
-		elif wavelength >= 580 and wavelength <= 645:
-			R = 1.0
-			G = (-(wavelength - 645) / (645 - 580)) ** gamma
-			B = 0.0
-		elif wavelength >= 645 and wavelength <= 750:
-			attenuation = 0.3 + 0.7 * (750 - wavelength) / (750 - 645)
-			R = (1.0 * attenuation) ** gamma
-			G = 0.0
-			B = 0.0
-		else:
-			R = 0.0
-			G = 0.0
-			B = 0.0
-		R *= 255
-		G *= 255
-		B *= 255
-		return (int(R), int(G), int(B))
-		
+
+    def soundAnalyze(self, stream):
+        data = stream.read(8192, exception_on_overflow=False)
+        waveData = wave.struct.unpack("%dh"%(8192), data)
+        npArrayData = np.array(waveData)
+        volume = npArrayData[1:].argmax() + 1
+        fftData=np.abs(np.fft.rfft(npArrayData))
+        which = fftData[1:].argmax() + 1
+        thefreq = which*44100/8192
+        return[npArrayData[volume], thefreq]
+
     def __musicLoop(self):
-	freqArray = [0, 0, 0, 0, 0]
-	freqCounter = 0
-	lightWave = 0
-	targetFreq = 0
-	p = pyaudio.PyAudio()
-	stream = p.open(format = pyaudio.paInt16, channels = 1, rate = 44100, input=True, 
-						frames_per_buffer = 8192)
-        while True:
-            self.__e.wait()
-            # TODO: do the boogy
-            # While in this loop use local colour/brightness declaration not the self.colour, self.brightness
-            # Use brightnessAdjustedColour for proper values if needed.
-			soundData = soundAnalyze(stream)
-			
-			if (soundData[1] > 100) and (soundData[1] < 880):
-				freqArray[freqCounter] = soundData[1]
-				freqCounter += (freqCounter + 1) % 5
-			
-			for i in freqArray:
-				targetFreq += freqArray[i]
-			print(targetFreq)
-			if (targetFreq > 100) and (targetFreq < 880):
-				lightWave = targetFreq * (37/78) + 333
-			targetRgb = wavelength_to_rgb(lightWave)
-			currentRgb = self.colour
-			#tuple([sum(x) for x in zip(a,b)]) 
-			while (currentRgb != targetRgb):
-				for i in range(3):
-					if (currentRgb[i] - targetRgb[i] <= 10) and (currentRgb[i] - targetRgb[i] >= -10):
-						currentRgb[i] = targetRgb[i]
-					else:
-						if (currentRgb[i] < targetRgb[i]):
-							currentRgb[i] += 10
-						else:
-							currentRgb[i] -= 10
-				time.sleep(0.01)
-					
-				
-			
-            pass
-	stream.stop_stream()
-	stream.close()
-	p.terminate()
+        freqArray = [0, 0, 0, 0, 0]
+        freqCounter = 0
+        lightWave = 0
+        targetFreq = 0
+        p = pyaudio.PyAudio()
+        stream = p.open(format = pyaudio.paInt16, channels = 1, rate = 44100, input=True,
+                            frames_per_buffer = 8192)
+        try:
+            while True:
+                self.__e.wait()
+                # TODO: do the boogy
+                # While in this loop use local colour/brightness declaration not the self.colour, self.brightness
+                # Use brightnessAdjustedColour for proper values if needed.
+                while self.__e.isSet():
+                    soundData = self.soundAnalyze(stream)
+                    if 100 < soundData[1] < 880:
+                        freqArray[freqCounter] = soundData[1]
+                        freqCounter += (freqCounter + 1) % 5
+                    for i in freqArray:
+                        targetFreq += freqArray[i]
+                    if 100 < targetFreq < 880:
+                        lightWave = targetFreq * (37/78) + 333
+
+                    targetRGB = list(wavelength_to_rgb(lightWave))
+                    currentRGB = list(self.colour)
+                    # difference = tuple(((x-y) / 10) for x, y in zip(targetRGB, currentRGB))
+                    while currentRGB != targetRGB:
+                        for i in range(3):
+                            if 10 >= currentRGB[i] - targetRGB[i] >= -10:
+                                currentRGB[i] = targetRGB[i]
+                            else:
+                                if currentRGB[i] < targetRGB[i]:
+                                    currentRGB[i] += 10
+                                else:
+                                    currentRGB[i] -= 10
+                        self.setColour(tuple(currentRGB))
+                        self.show_colours()
+                        time.sleep(0.02)
+        except Exception:
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
