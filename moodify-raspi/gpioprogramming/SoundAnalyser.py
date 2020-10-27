cf27c072da93464e661ade55fd52bc4a9c8c3849
Puru Vaish <puruvaish24@outlook.com>
2020-10-27 18:17:00 +0100

    Adjust: the configuration os SoundAnalyser.py

--- moodify-raspi/gpioprogramming/SoundAnalyser.py
+++ moodify-raspi/gpioprogramming/SoundAnalyser.py
@@ -6,6 +6,7 @@ from scipy.signal import butter, sosfilt
 
 def wavelength_to_rgb(wavelength, gamma=0.8):
     wavelength = float(wavelength)
+    wavelength = wavelength/3 + 340
     if 380 <= wavelength <= 440:
         attenuation = 0.3 + 0.7 * (wavelength - 380) / (440 - 380)
         R = ((-(wavelength - 440) / (440 - 380)) * attenuation) ** gamma
@@ -30,12 +31,9 @@ def wavelength_to_rgb(wavelength, gamma=0.8):
     elif 645 <= wavelength <= 750:
         attenuation = 0.3 + 0.7 * (750 - wavelength) / (750 - 645)
         R = (1.0 * attenuation) ** gamma
-        G = 0.0
-        B = 0.0
+        G = B = 0.0
     else:
-        R = 0.0
-        G = 0.0
-        B = 0.0
+        R = G = B = 0.0
     R *= 255
     G *= 255
     B *= 255
@@ -45,11 +43,11 @@ def wavelength_to_rgb(wavelength, gamma=0.8):
 class SignalAnalyser:
 
     def __init__(self):
-        self.lowcut = 150
-        self.highcut = 850
+        self.lowcut = 80
+        self.highcut = 920
         self.fs = 44100
         self.order = 10
-        self.CHUNK = 4 * 4096
+        self.CHUNK = 4096
         self.p = pyaudio.PyAudio()  # start the PyAudio class
         self.stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=self.fs, input=True,
                                   frames_per_buffer=self.CHUNK)  # uses default input device
@@ -85,7 +83,7 @@ class SignalAnalyser:
         _, _, peak_req, amp = self.analyse_data(data)
         adjustedAMP = 0
         if amp > 2:
-            adjustedAMP = max(min(amp/30, 1), 0.1)
+            adjustedAMP = max(min(amp/20, 1), 0.1)
         return peak_req, adjustedAMP
 
     def terminate(self):
@@ -101,4 +99,4 @@ if __name__ == '__main__':
         thisFreq, thisAmp = newAnalysis.get_next_pair()
         average = thisFreq * weight + (1 - weight) * lastFreq
         lastFreq = thisFreq
-        print((average, thisAmp))
+        print((round(average), thisAmp, wavelength_to_rgb(average)))
